3c3cd79bce81994e281e56265d2ff411dde10be1
22913264-sketch <22913264@unilasallelucas.edu.br>
2026-05-19 20:58:34 -0400

    Tela Login arrumada

--- app/src/main/java/com/example/joia2026/LoginActivity.kt
+++ app/src/main/java/com/example/joia2026/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -16,7 +17,7 @@ class LoginActivity : AppCompatActivity() {
         val etEmail = findViewById<EditText>(R.id.etEmail)
         val etSenha = findViewById<EditText>(R.id.etSenha)
         val btnLogin = findViewById<Button>(R.id.btnLogin)
-        val btnIrCadastro = findViewById<Button>(R.id.btnIrCadastro)
+        val btnIrCadastro = findViewById<TextView>(R.id.btnIrCadastro)
 
         btnLogin.setOnClickListener {
             val email = etEmail.text.toString()
